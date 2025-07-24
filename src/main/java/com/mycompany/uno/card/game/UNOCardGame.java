9e20f030aaa255e733778c8455199493929badb0
ChaulaDevi <ChaulaDevi@localhost>
2025-07-24 19:29:48 -0400

    initial-placeholder-2

---
+++ src/main/java/com/mycompany/uno/card/game/UNOCardGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uno.card.game;
+
+/**
+ *
+ * @author ChaulaDevi
+ */
+public class UNOCardGame {
+
+    public static void main(String[] args) {
+        System.out.println("UNO Game placeholder");
+    }
+}
